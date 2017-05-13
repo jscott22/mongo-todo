802bca5bb3a93d609fb1e9e23678e8d04e18df06
Jason Scott <dev.jason.scott@gmail.com>
2017-05-13 20:06:11 +0200

    adds test cases

--- server/models/todo.js
+++ server/models/todo.js
@@ -4,7 +4,6 @@
 
 let mongoose = require('mongoose');
 
-
 let Todo = mongoose.model('Todo', {
     text: {
         type: String,
--- server/server.js
+++ server/server.js
@@ -4,12 +4,12 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 
-
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
 
 let app = express();
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
@@ -17,6 +17,7 @@ app.listen(3000, () => {
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
+
     let todo = new Todo({
         text: req.body.text
     });
@@ -26,25 +27,6 @@ app.post('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
-
 });
 
-// let newTodo = new Todo({
-//     text: 'Hug Oscar',
-// });
-
-let newUser = new User({
-
-});
-
-// newTodo.save().then((doc) => {
-//     console.log('Saved todo', doc);
-// }, (e) => {
-//     console.log('Unable to save todo', e);
-// });
-
-// newUser.save().then((doc) => {
-//     console.log('Saved user', doc);
-// }, (e) => {
-//     console.log('Unable to save user', e);
-// });
+module.exports = {app};
